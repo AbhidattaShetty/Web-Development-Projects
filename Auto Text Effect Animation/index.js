da2c05b1035deaee0ac903511bafc3c550b3edf6
AbhidattaShetty <108416589+AbhidattaShetty@users.noreply.github.com>
2022-06-29 20:38:34 +0530

    Add files via upload

---
+++ Auto Text Effect Animation/index.js
@@ -0,0 +1,31 @@
+const containerEl = document.querySelector(".container");
+
+const careers = ["YouTuber", "Web Developer", "Cricketer", "Traveller"]
+
+let careerIndex = 0;
+let characterIndex=0;
+
+function updateText(){
+    characterIndex++
+    containerEl.innerHTML =`
+<h1>I am an ${careers[careerIndex].slice(0,characterIndex)}</h1>
+`;
+
+
+if (characterIndex === careers[careerIndex].length) {
+    careerIndex++;
+    characterIndex = 0;
+  }
+
+if (careerIndex === careers.length) {
+    careerIndex = 0;
+  }
+
+setTimeout(() => {
+    updateText()
+},300)
+}
+
+updateText();
+
+
